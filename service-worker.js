1bacd24d593524278d5170e19bf782d0b6a9683b
GobboSW <168787591+GobboSW@users.noreply.github.com>
2024-05-31 13:37:58 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,31 @@
 self.addEventListener('install', (event) => {
     console.log('Service worker installing...');
     // Here you would cache files
+    event.waitUntil(
+        caches.open('static-v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/manifest.json',
+                '/MyIcon-192.png',
+                '/MyIcon-512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activating...');
 });
 
 self.addEventListener('fetch', (event) => {
     console.log('Fetching:', event.request.url);
     // Here you can control how to respond to requests
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
+
